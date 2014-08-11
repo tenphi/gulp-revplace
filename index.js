e0733af6bbee4719bb3e963685da38863c08c129
Andrey Yamanov <tenphi@gmail.com>
2014-08-11 19:47:29 +0400

    plugin completely remastered

--- index.js
+++ index.js
@@ -1,169 +1,98 @@
 
 var path = require('path'),
   through = require('through2'),
-  split = require('split2'),
   gutil = require('gulp-util'),
   chalk = require('chalk'),
-  crypto = require('crypto');
+  magic = new (require('mmmagic').Magic)();
 
-var PLUGIN_NAME = 'gulp-revplacer';
+var PLUGIN_NAME = 'gulp-revplace';
 
 module.exports = function(opt) {
 
+  /**
+   * @type {{regex,addPrefix,stripPrefix}}
+   */
   var opt = opt || {};
 
-  var regex = opt.regex || /(?:url\(["']?(.*?)['"]?\)|src=["'](.*?)['"]|src=([^\s\>]+)(?:\>|\s)|href=["'](.*?)['"]|href=([^\s\>]+)(?:\>|\s))/g;
-  var template = opt.template || '{name}-{hash}.{ext}';
-  var assetPath = opt.assetPath != null ? String(opt.assetPath) : '';
-  var urlPrefix = opt.urlPrefix != null ? String(opt.urlPrefix) : '/';
-  var flatten = !!opt.flatten;
-  var passAll = !!opt.passAll;
-  var hashLength = opt.hashLength && opt.hashLength >= 4 && opt.hashLength <= 32 ? parseInt(opt.hashLength) : 8;
+  var regex = opt.regex || /(?:url\(["']?(.*?)['"]?\)|src=["'](.*?)['"]|src=([^\s\>]+)(?:\>|\s)|href=["'](.*?)['"]|href=([^\s\>]+)(?:\>|\s)|ASSET\(['"](.+)['"]\))/g;
+  var addPrefix = opt.addPrefix != null ? String(opt.addPrefix) : '/';
+  var stripPrefix = opt.stripPrefix != null ? String(opt.stripPrefix).replace(/^\//, '') : '';
+  var skipUnmentioned = opt.skipUnmentioned != null ? !!opt.skipUnmentioned : true;
   var assets = [];
 
-  var buffer = [];
-  var sourcesLoaded = false,
-    assetsLoaded = false,
-    context = null;
-
-  if (opt.assets && typeof(opt.assets.on) === 'function' && typeof(opt.assets.write) === 'function') {
-    opt.assets.pipe(through.obj(generateContentHandler('asset'), function() {
-      assetsLoaded = true;
-      endStream();
-    }));
-  }
-
-  function getBaseDir(file) {
-    return file.origPath.split(file.origBase).slice(0, -1).join('');
-  }
-
-  function addAssetToOutput(asset, direct) {
-    if (direct) {
-      context.push(asset);
-      return;
+  function fileHandler(file, enc, cb) {
+    if (file.isNull()) return; // ignore
+    if (file.isStream()) {
+      throw new gutil.PluginError(PLUGIN_NAME,  'Streaming not supported');
     }
-    var basename = path.basename(asset.path);
-    if (!asset.added) {
-      var collision = false;
-      assets.forEach(function(asset2) {
-        if (asset2.added && path.basename(asset2.origPath) === basename) {
-          collision = true;
-        }
-      });
-      if (!collision) {
-        var tmpPath = getFileNameWithHash(asset);
-        if (assetPath) {
-          tmpPath = path.join(assetPath, path.basename(tmpPath));
-        }
-        asset.path = path.join(getBaseDir(asset), tmpPath);
-        asset.base = '.';
-        basename = path.basename(asset.path);
-        context.push(asset);
-      }
-      asset.added = true;
-    }
-    return assetPath ? path.join(assetPath, basename) : basename;
-  }
 
-  function getFileNameWithHash(file) {
-    var filepath = file.origPath || file.path;
-    var content = String(file.contents);
-    var hash = crypto
-      .createHash('md5')
-      .update(content, 'utf8')
-      .digest('hex')
-      .slice(0, hashLength);
-    var basename = path.basename(filepath);
-    return template
-      .replace('{hash}', hash)
-      .replace('{name}', basename.slice(0, basename.lastIndexOf('.')))
-      .replace('{ext}', path.extname(basename).slice(1));
-  }
-
-  function generateContentHandler(type, cb) {
-    return function(file, enc, cb) {
-      if (file.isNull()) return; // ignore
-      if (file.isStream()) {
-        throw new gutil.PluginError(PLUGIN_NAME,  'Streaming not supported');
-      }
-
-      file.origPath = file.origPath || file.path;
-      file.origBase = file.origBase || file.base;
-      file.origCwd = file.origCwd || file.cwd;
-
-      file.type = type;
-      if (type) {
-        assets.push(file);
-      }
-      file.added = false;
-      buffer.push(file);
-
-      if (type === 'asset' && passAll) {
-        addAssetToOutput(file, true);
-      }
-
+    assets.push(file);
+    magic.detect(file.contents, function(err, result) {
+      if (err) throw new gutil.PluginError(PLUGIN_NAME,  'Wrong content type of file `' + file.path + '`. ' + err);
+      file.isTextType = !!~result.indexOf('ASCII');
       cb();
-    }
+    });
+  }
+
+  function getNormalPath(path, base) {
+    return path.split(base).slice(-1)[0].slice(1);
   }
 
   function endStream() {
-    if (sourcesLoaded && assetsLoaded) {
-      replaceUrls();
-    }
+    replaceUrls(this);
   }
 
-  function replaceUrls() {
-    var sources = buffer.filter(function(file) {
-      return file.type === 'source';
-    });
-    var assetsMap = {};
-    var assets = buffer.filter(function(file) {
-      var isAsset = file.type === 'asset';
-      if (isAsset) {
-        assetsMap[file.path] = file;
-        file.cwd = path.dirname(file.path);
-        file.base = '.';
-      }
-      return isAsset;
+  function replaceUrls(context) {
+    var sources = assets.filter(function(file) {
+      return file.isTextType;
     });
 
     sources.forEach(function(file) {
       var content = String(file.contents);
+
       content = content.replace(regex, function(str) {
         var found = Array.prototype.slice.call(arguments, 1).filter(function(a) {return a;})[0];
         var url = found.replace(/^\//, '');
+        if (stripPrefix && stripPrefix === url.slice(0, stripPrefix.length)) {
+          url = url.replace(stripPrefix, '');
+        }
         assets.forEach(function(asset) {
-          if (path.basename(asset.origPath) !== path.basename(url)) {
+          if (asset === file || !asset.revOrigPath) {
             return;
           }
 
-          var absolutePath = asset.origPath
-            .replace(path.join(asset.origCwd, asset.origBase), '')
-            .replace(asset.origBase, '')
-            .replace(/^\//, '');
-          var relativePath = path.relative(path.dirname(file.origPath), asset.origPath);
+          var oldPath = getNormalPath(asset.revOrigPath, asset.revOrigBase);
+          var newPath = getNormalPath(asset.path, asset.base);
+
+          if (path.basename(oldPath) !== path.basename(url)) {
+            return;
+          }
+
+          var absolutePath = oldPath;
+          var relativePath = path.relative(path.dirname(file.revOrigPath || file.path), asset.revOrigPath);
 
           if (absolutePath === url || relativePath === url) {
-            var tmpPath = addAssetToOutput(asset);
-            var replaced = urlPrefix + tmpPath;
+            if (skipUnmentioned)
+              context.push(asset);
+            var replaced = addPrefix + newPath;
             str = str.replace(found, replaced);
           }
         });
         return str;
       });
       file.contents = new Buffer(content);
-      if (flatten) {
-        file.base = path.dirname(file.path);
-      }
-      context.push(file);
+      if (skipUnmentioned)
+        context.push(file);
     });
+
+    if (!skipUnmentioned) {
+      assets.forEach(function(asset) {
+        context.push(asset);
+      });
+    }
 
     context.emit('end');
   }
 
-  return through.obj(generateContentHandler('source'), function() {
-    context = this;
-    sourcesLoaded = true;
-    endStream();
-  });
+  return through.obj(fileHandler, endStream);
 };
--- test/match/script.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var absoluteImageUrl = $ASSET('/assets/cat-9121e197.png');
-  var relativeImageUrl = $ASSET('/assets/cat-9121e197.png');
-
-  var absoluteImageUrl2 = $ASSET('/assets/zombie-b52ed4d8.png');
-})();
